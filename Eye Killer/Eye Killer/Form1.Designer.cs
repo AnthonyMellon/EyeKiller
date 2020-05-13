26b3e75c7b280e654ccae0a0fba1b3695253c788
AnthonyMellon <Anthonymellon@yahoo.com>
2020-05-13 16:53:20 +1200

    visual redesign. also displaying faces x and y position

--- Eye Killer/Eye Killer/Form1.Designer.cs
+++ Eye Killer/Eye Killer/Form1.Designer.cs
@@ -30,55 +30,79 @@
         {
             this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxFaceX = new System.Windows.Forms.TextBox();
+            this.textBoxFaceY = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(18, 166);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1164, 508);
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(18, 18);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(1164, 138);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(519, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(131, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Face Position";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxFaceX
+            // 
+            this.textBoxFaceX.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxFaceX.Location = new System.Drawing.Point(519, 51);
+            this.textBoxFaceX.Name = "textBoxFaceX";
+            this.textBoxFaceX.Size = new System.Drawing.Size(131, 13);
+            this.textBoxFaceX.TabIndex = 3;
+            // 
+            // textBoxFaceY
+            // 
+            this.textBoxFaceY.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxFaceY.Location = new System.Drawing.Point(519, 77);
+            this.textBoxFaceY.Name = "textBoxFaceY";
+            this.textBoxFaceY.Size = new System.Drawing.Size(131, 13);
+            this.textBoxFaceY.TabIndex = 4;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(679, 530);
+            this.Controls.Add(this.textBoxFaceY);
+            this.Controls.Add(this.textBoxFaceX);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxFaceX;
+        private System.Windows.Forms.TextBox textBoxFaceY;
     }
 }
 
--- Eye Killer/Eye Killer/Form1.cs
+++ Eye Killer/Eye Killer/Form1.cs
@@ -32,16 +32,27 @@ namespace Eye_Killer
 
         public void DetectFaceHaar()
         {
+            //get the cascade classifier
             string facePath = Path.GetFullPath(@"../../data/haarcascade_frontalface_default.xml");
             CascadeClassifier classifierFace = new CascadeClassifier(facePath);
+
+            //find faces in the current image
             var imgGray = imgInput.Convert<Gray, byte>().Clone();
             Rectangle [] faces = classifierFace.DetectMultiScale(imgGray, 1.1, 4);
 
-            foreach(var face in faces)
-            {
-                imgInput.Draw(face, new Bgr(0, 0, 255), 2);
-                imgInput.Draw(new CircleF(new PointF(face.X + (face.Width / 2), face.Y + (face.Height / 2)), 1), new Bgr(0, 255, 0), 2);
-            }
+            //Get center point of face
+            float faceCenterX = faces[0].X + faces[0].Width / 2;
+            float faceCenterY = faces[0].Y + faces[0].Height / 2;
+
+            //Display faces x and y position
+            textBoxFaceX.Text = "X: " + faceCenterX;
+            textBoxFaceY.Text = "Y: " + faceCenterY;
+
+            //Draw rectangle and circle over first face
+            imgInput.Draw(faces[0], new Bgr(0, 0, 255), 2);
+            imgInput.Draw(new CircleF(new PointF(faceCenterX, faceCenterY), 1), new Bgr(0, 255, 0), 2);
+
+            //set the picture box image
             pictureBox1.Image = imgInput.AsBitmap();
         }
     }
